11cd0b7f6d87f345ac34e7f132a4cf7d3c1bd714
olivmai <oliviermairet@gmail.com>
2020-12-17 15:24:46 +0100

    feat(database): add db client and first model for information source

---
+++ src/Domain/InformationSource/Model/InformationSource.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Domain\InformationSource\Model;
+
+class InformationSource
+{
+    private string $title;
+    private string $url;
+    private string $image;
+    private string $description;
+
+    public function __construct(string $title, string $url, string $image, string $description)
+    {
+        $this->title = $title;
+        $this->url = $url;
+        $this->image = $image;
+        $this->description = $description;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+}
